bceb324da276df93b44e5700e3844b846ee5fa9d
Lokesh Lalwani <lokesh.lalwani201@gmail.com>
2025-04-29 11:32:35 -0400

    app.py

--- app.py
+++ app.py
@@ -5,7 +5,17 @@ import plotly.express as px
 import sweetviz as sv
 import sqlite3
 import sqlalchemy
-from db import create_connection, create_user_table, add_user, check_user, get_user_by_username, create_workspace_table, create_dashboard_tables
+from db import (
+    create_connection,
+    create_user_table,
+    create_workspace_table,
+    create_dashboard_tables,
+    create_dashboard_sharing_and_history,
+    create_comments_table,
+    add_user,
+    check_user,
+    get_user_by_username,
+)
 
 # Initialize session state
 if "auth_mode" not in st.session_state:
